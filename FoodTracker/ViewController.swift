75790caa035a41392239e7a8dc74401284d5d9aa
Nadzeya <Nadezhda.Leonovich@gmail.com>
2019-08-07 22:47:21 +0300

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Nadzeya Leanovich on 8/7/19.
+//  Copyright © 2019 Nadzeya Leanovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
